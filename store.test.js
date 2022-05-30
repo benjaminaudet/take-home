ab1309d3339324d502d3b8c4199eaa16743ea4d0
benjamin <benjamin@audet.fr>
2022-05-30 17:19:21 +0200

    Update tests + add Naturalia test 1

--- store.test.js
+++ store.test.js
@@ -2,18 +2,25 @@ import { Store } from "./store";
 import { DiscountOffer } from "./DiscountOffer"
 
 describe("Store Partners Discount Offers", () => {
-  it("should decrease the discount and expiresIn", () => {
+  it("[Global] should decrease the discount and expiresIn", () => {
     expect(
       JSON.stringify(new Store([new DiscountOffer("test", 2, 3)]).updateDiscounts())
     ).toEqual(
       JSON.stringify([new DiscountOffer("test", 1, 2)])
     );
   });
-  it("should decrease twice as fast when the discount offer expires", () => {
+  it("[Global] should decrease twice as fast when the discount offer expires", () => {
     expect(
       JSON.stringify(new Store([new DiscountOffer("test", -1, 3)]).updateDiscounts())
     ).toEqual(
       JSON.stringify([new DiscountOffer("test", -2, 1)])
+    );
+  });
+  it("[Naturalia] should increase the older it gets", () => {
+    expect(
+      JSON.stringify(new Store([new DiscountOffer("Naturalia", 10, 3, false, true)]).updateDiscounts())
+    ).toEqual(
+      JSON.stringify([new DiscountOffer("Naturalia", 9, 4, false, true)])
     );
   });
 });
